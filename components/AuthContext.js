7971bac6ba5edd34e1146d053f9ae90a45ddf6e0
UO225811 <UO225811@uniovi.es>
2023-11-15 13:58:05 +0100

    updateBalance Added

--- components/AuthContext.js
+++ components/AuthContext.js
@@ -1,4 +1,6 @@
 import React, { createContext, useState, useContext } from 'react';
+import { firestore } from "../database/firebase";
+import { getDoc, doc } from "@firebase/firestore";
 
 const AuthContext = createContext();
 
@@ -7,7 +9,23 @@ export const AuthProvider = ({ children }) => {
 
   const login = (userData) => {
     setUser(userData);
-    // console.log(userData);
+  };
+
+  const updateBalance = async (theId, tableName) => {
+    const docRef = doc(firestore, tableName, theId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const userDetails = {
+        id: theId,
+        name: docSnap.data().name,
+        email: docSnap.data().email,
+        phone: docSnap.data().phone,
+        balance: docSnap.data().balance,
+      };
+      setUser(userDetails);                
+    } else {
+      console.log("No such user!");
+    }
   };
 
   const logout = () => {
@@ -18,6 +36,7 @@ export const AuthProvider = ({ children }) => {
     user,
     login,
     logout,
+    updateBalance
   };
 
   return (
